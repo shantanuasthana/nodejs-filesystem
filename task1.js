10a33c9136d093208f9f3da1e87524545da2668f
shantanuasthana <43820814+shantanuasthana@users.noreply.github.com>
2021-05-20 14:00:11 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const fs = require("fs");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+app.use(express.json());
+
+var date = new Date();
+
+//File Write operation
+app.post("/write-file", function(req,res){
+
+    fs.writeFileSync("./TextFiles/"+date.getUTCDate()+".txt", date, function(err){
+        if(err) throw err;
+    });
+    res.json({
+        message:"File created",
+    });
+});
+
+//Read Operation
+app.get("/get-all-text-files", function(req,res){
+    let listOfFiles = [];
+    fs.readdirSync("./TextFiles").forEach((file)=>{
+        listOfFiles.push(file);
+    });
+    if (listOfFiles.length === 0) {
+        res.send("No files found");
+      } else {
+        res.send(listOfFiles);
+      }
+    });
+
+//Server
+app.get("/", (req, res) => {
+    res.send("File operations using node js");
+  });
+
+app.listen(PORT, () => {
+    console.log("server is up and running");
+  });
